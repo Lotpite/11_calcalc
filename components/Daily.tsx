a0267d200a9c16d5163bd3558b1e0eba5a985d0a
Lotpite <exstornado@gmail.com>
2023-05-30 13:39:52 +0300

    animation of daily

--- components/Daily.tsx
+++ components/Daily.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 import Progress from './Progress'
 
 const Daily = () => {
+
+  const [appear, setAppear] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAppear(true)
+    }, 100)
+  }, [])
+
   return (
-    <div className='flex flex-col justify-center w-full items-center md:gap-18 gap-10'>
+    <div className='flex flex-col justify-center w-full items-center'>
       <div className='flex flex-row flex-row justify-around items-center md:gap-20 gap-5'>
         <div className='flex flex-col items-center'>
           <h5 className='text-xl text-gray-400'>Left</h5>
@@ -16,12 +27,20 @@ const Daily = () => {
         </div>
       </div>
 
-      <div className='flex flex-row justify-center w-5/6 lg:gap-36 md:gap-20 sm:gap-14 gap-8 items-center md:mt-10'>
+      <div className='flex flex-row justify-around w-5/6  items-center md:mt-15 xl:mt-5 lg:mt-16 sm:mt-20 mt-14'>
         <div className='flex flex-col items-center justify-center'>
-          <h5 className='text-md text-gray-400'>Breakfast</h5>
-          <p className='text-sm text-green-500 font-semibold'>400</p>
+          <h5 className={`text-md text-gray-400 transition duration-75 ease-in-out transform ${appear ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'}`}>Breakfast</h5>
+          <p className={`text-sm text-green-500 font-semibold transition delay-500 duration-75 ease-in-out transform ${appear ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}>400</p>
         </div>
-        <div className='flex flex-col items-center'>
+        <div className='flex flex-col items-center justify-center'>
+          <h5 className={`text-md text-gray-400 transition duration-75 delay-200 ease-in-out transform ${appear ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'}`}>Breakfast</h5>
+          <p className={`text-sm text-green-500 font-semibold transition delay-700 duration-75 ease-in-out transform ${appear ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}>400</p>
+        </div>
+        <div className='flex flex-col items-center justify-center'>
+          <h5 className={`text-md text-gray-400 transition duration-75 delay-300 ease-in-out transform ${appear ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'}`}>Breakfast</h5>
+          <p className={`text-sm text-green-500 font-semibold transition delay-1000 duration-75 ease-in-out transform ${appear ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}>400</p>
+        </div>
+        {/* <div className='flex flex-col items-center'>
           <h5 className='text-md text-gray-400'>Dinner</h5>
           <p className='text-sm text-green-500 font-semibold'>600</p>
         </div>
@@ -32,7 +51,7 @@ const Daily = () => {
         <div className='flex flex-col items-center'>
           <h5 className='text-md text-gray-400'>Snacks</h5>
           <p className='text-sm text-green-500 font-semibold'>200</p>
-        </div>
+        </div> */}
       </div>
     </div>
   )
